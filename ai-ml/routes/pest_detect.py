a17f5c1d8f4f83fca253bb2b8fb2bf4d22e8bccc
VinaySrigadi <vsrigadil@gmail.com>
2026-02-21 19:27:56 +0530

    Switch to TFLite for Render deployment

--- ai-ml/routes/pest_detect.py
+++ ai-ml/routes/pest_detect.py
@@ -1,13 +1,17 @@
 from flask import Blueprint, request, jsonify
 from PIL import Image
 import numpy as np
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 import json
 import io
 
 pest_bp = Blueprint('pest', __name__)
 
-model = tf.keras.models.load_model('models/pest_model.h5')
+interpreter = tflite.Interpreter(model_path='models/pest_model.tflite')
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 with open('models/class_labels.json', 'r') as f:
     class_labels = json.load(f)
@@ -19,12 +23,15 @@ def detect_pest():
 
     file = request.files['image']
     img = Image.open(io.BytesIO(file.read())).resize((224, 224))
-    img_array = np.array(img) / 255.0
+    img_array = np.array(img, dtype=np.float32) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    predictions = model.predict(img_array)
-    top_index = np.argmax(predictions[0])
-    confidence = round(float(predictions[0][top_index]) * 100, 2)
+    interpreter.set_tensor(input_details[0]['index'], img_array)
+    interpreter.invoke()
+
+    predictions = interpreter.get_tensor(output_details[0]['index'])[0]
+    top_index = np.argmax(predictions)
+    confidence = round(float(predictions[top_index]) * 100, 2)
     disease = class_labels[str(top_index)]
 
     return jsonify({
